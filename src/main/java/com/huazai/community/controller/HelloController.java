378a47ae8c0153248c01197feb105b4fa9ac791d
huazai <1285302070@qq.com>
2019-09-30 20:03:53 +0800

    init repo

---
+++ src/main/java/com/huazai/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.huazai.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/helloww")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
