673555fa754f802ea155198a4e4131ce3d4af020
huazai <1285302070@qq.com>
2019-10-02 15:25:15 +0800

    aa

--- src/main/java/com/huazai/community/CommunityApplication.java
+++ src/main/java/com/huazai/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package com.huazai.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.huazai.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/huazai/community/controller/AuthorizeController.java
+++ src/main/java/com/huazai/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.huazai.community.controller;
 
 import com.huazai.community.dto.AccessTokenDTO;
 import com.huazai.community.dto.GithubUser;
+import com.huazai.community.mapper.UserMapper;
+import com.huazai.community.model.User;
 import com.huazai.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -24,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")//请求路径
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){//获取请求地址栏传回的参数
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){//获取请求地址栏传回的参数
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -33,10 +42,27 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user =  githubProvider.getUser(accessToken);
+        GithubUser githubUser =  githubProvider.getUser(accessToken);//获取用户信息
         System.out.println("=====================");
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        return "index";//返回界面
+        System.out.println(githubUser.getName());
+        System.out.println(githubUser.getId());
+        if (githubUser != null){
+            //登陆成功
+            //设置user信息
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);//将用户信息放到session中
+            return "redirect:/";//返回界面(重定向)
+        }else{
+            return "redirect:/";//返回界面(重定向)
+            //登陆失败
+
+        }
+
     }
 }
